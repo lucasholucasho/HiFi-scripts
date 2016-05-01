420e7e09a95c5c7dfc6e1ae77550382f263d6030
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 12:42:08 -0700

    script.load

--- loadScriptsOnClick.js
+++ loadScriptsOnClick.js
@@ -2,7 +2,7 @@
 	var controllerMouse = "http://howard-stearns.github.io/models/scripts/handControllerPointer.js";
 	var moveBox = "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/movetextbox.js";
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
-		Script.loadURL(controllerMouse, true);
-		Script.loadURL(moveBox, true); 
+		Script.load(controllerMouse);
+		Script.load(moveBox); 
     }; 	
 })
