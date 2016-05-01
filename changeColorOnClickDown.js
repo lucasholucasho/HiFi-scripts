2c4789365fd0343077c2d62e71d7af9dd7fee0aa
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 01:36:24 -0700

    Hard code id

--- changeColorOnClickDown.js
+++ changeColorOnClickDown.js
@@ -6,10 +6,10 @@
     var clicked = false;
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
         if (clicked){
-            Entities.editEntity(skyboxId, { skybox: { color: { red: 255, green: 255, blue: 255}, url: "http://schmedia.com/wp-content/uploads/2015/04/SKYPAX-_0008_NoCal-Coast-Sunset.jpg"} });
+            Entities.editEntity("{7626c954-6ade-41b4-a1be-411cfdf1241c}", { skybox: { color: { red: 255, green: 255, blue: 255}, url: "http://schmedia.com/wp-content/uploads/2015/04/SKYPAX-_0008_NoCal-Coast-Sunset.jpg"} });
             clicked = false;
         }else{
-			Entities.editEntity(skyboxId, { skybox: { color: { red: 255, green: 255, blue: 255}, url: "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/Cubemap_Test_01.jpg"} });            clicked = true; 
+			Entities.editEntity("{7626c954-6ade-41b4-a1be-411cfdf1241c}", { skybox: { color: { red: 255, green: 255, blue: 255}, url: "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/Cubemap_Test_01.jpg"} });            clicked = true; 
         }
     }; 
 })
