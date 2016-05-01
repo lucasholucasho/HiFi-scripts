c83cdaafccd012adce2894fba2980a73425079c6
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 12:32:48 -0700

    attach the click event

--- loadScriptsOnClick.js
+++ loadScriptsOnClick.js
@@ -1,6 +1,8 @@
 (function(){ 
 	var controllerMouse = "http://howard-stearns.github.io/models/scripts/handControllerPointer.js";
 	var moveBox = "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/movetextbox.js";
-	Script.loadURL(controllerMouse, true);
-	Script.loadURL(moveBox, true);	
+    this.clickDownOnEntity = function(entityID, mouseEvent) { 
+		Script.loadURL(controllerMouse, true);
+		Script.loadURL(moveBox, true); 
+    }; 	
 })
