48084429b315f9390f0ac91f53ebbe3b1b970d22
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 01:32:36 -0700

    Hopefully change skybox image when we click

--- changeColorOnClickDown.js
+++ changeColorOnClickDown.js
@@ -1,15 +1,15 @@
 (function(){ 
 	var skyboxId = "{7626c954-6ade-41b4-a1be-411cfdf1241c}";
 	var skyboxProperties = Entities.getEntityProperties(skyboxId);
-    var xPosition = JSON.stringify(sProperties);
+    var xPosition = JSON.stringify(skyboxProperties);
+
     var clicked = false;
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
         if (clicked){
-            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            Entities.editEntity(skyboxId, { skybox: { color: { red: 255, green: 255, blue: 255}, url: "http://schmedia.com/wp-content/uploads/2015/04/SKYPAX-_0008_NoCal-Coast-Sunset.jpg"} });
             clicked = false;
         }else{
-            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
-            clicked = true; 
+			Entities.editEntity(skyboxId, { skybox: { color: { red: 255, green: 255, blue: 255}, url: "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/Cubemap_Test_01.jpg"} });            clicked = true; 
         }
     }; 
 })
