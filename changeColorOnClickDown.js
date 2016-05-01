54a77787427bd4f7b4b51e7e84720ec7cf889c44
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 01:15:11 -0700

    Change color on click down

---
+++ changeColorOnClickDown.js
@@ -0,0 +1,12 @@
+(function(){ 
+    var clicked = false;
+    this.clickDownOnEntity = function(entityID, mouseEvent) { 
+        if (clicked){
+            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            clicked = false;
+        }else{
+            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
+            clicked = true; 
+        }
+    }; 
+})
