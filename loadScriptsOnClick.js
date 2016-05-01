cbf99915d5e86405cae72d36ee9d9dff4075aabd
lucasholucasho <lucasho2340@gmail.com>
2016-05-01 12:28:48 -0700

    loadURL()

--- loadScriptsOnClick.js
+++ loadScriptsOnClick.js
@@ -1,6 +1,6 @@
 (function(){ 
 	var controllerMouse = "http://howard-stearns.github.io/models/scripts/handControllerPointer.js";
 	var moveBox = "https://raw.githubusercontent.com/lucasholucasho/HiFi-scripts/master/movetextbox.js";
-	Script.load(controllerMouse);
-	Script.load(moveBox);	
+	Script.loadURL(controllerMouse);
+	Script.loadURL(moveBox);	
 })
